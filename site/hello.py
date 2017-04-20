5f101d7b0ef5b8c92f782f26f0b60a0f44eee1d7
Matt House <matthew.house89@gmail.com>
2017-04-20 16:04:26 -0400

    making pep8 happy.

--- site/hello.py
+++ site/hello.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def index():
+    return "Index"
+
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
